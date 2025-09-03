1fc883e23915ce2be854d09b024a26af45d5bb8f
XGizellHA <160786120+XGizellHA@users.noreply.github.com>
2025-09-02 19:03:16 -0600

    Update ej-01.py

--- practica-02/ej-01.py
+++ practica-02/ej-01.py
@@ -6,8 +6,14 @@ from utime import sleep_ms
 
 led = Pin(25, Pin.OUT)      
 
-while(True):                # Repeat forever
-    led.on()            
+def blink(timer): # Callback function
+    led.on()
     sleep_ms(1)
     led.off()
-    sleep_ms(5)
+    sleep_ms(20)
+
+timer = Timer() # Create the Timer object
+timer.init(freq=10000, # Timer frequency set to 2.5Hz
+    mode=Timer.PERIODIC, # Timer will run endlessly (not one-shot)
+    callback=blink # Set callback function: blink
+)
