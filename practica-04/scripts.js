f33a3a1fe36a0eea8e38b842b3bed481aa29c4fe
Rodrigo Tapia <rodrigo@pc-bdn-rtn.fi.unam>
2025-09-23 09:31:52 -0600

    agregados archivos base para la práctica 05

--- practica-04/scripts.js
+++ practica-04/scripts.js
@@ -1,7 +1,12 @@
+var marquee = null
 function deactivateAll(){
 	//var buttons = document.getElementsByTagName('button');
 	var buttons = document.querySelectorAll('button');
-  console.log(buttons.length)
+  console.log(marquee)
+	if(typeof(marquee) != "undefined" && marquee !== null){
+		clearInterval(marquee)
+		marquee = null
+	}
   for(let i = 0; i < buttons.length; i++){
 		buttons[i].classList.remove("on")
   }
@@ -10,6 +15,13 @@ function deactivateAll(){
 function activate(sender){
 	if(sender == null)
 		return;
+	if(sender.id == "mleft"){
+		marqueeleft();
+	}else if(sender.id == "mright"){
+		marqueeRight();
+	}else if(sender.id == "pingpong"){
+		pingPong();
+	}
 	sender.classList.add("on");
 }
 
@@ -28,3 +40,71 @@ function submit(action, value){
 		'value' :  value,
 	}));
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function marqueeRight(){
+	var buttons = document.querySelectorAll('[id=led]');
+	var i = 0;
+	marquee =  setInterval(() => {
+		//console.log("Mensaje cada 0.5 seg")
+		buttons[i].classList.add("on");
+		console.log(i);
+		if(i > 0){
+			buttons[i - 1].classList.remove("on");
+		}else{
+			buttons[buttons.length - 1].classList.remove("on");
+		}
+		i = i + 1 ;
+		if(i >= buttons.length){
+			i = 0;
+		}
+	}, 1000)
+
+}
+
+function marqueeleft(){
+	var buttons = document.querySelectorAll('[id=led]');
+	var i = buttons.length - 1;
+	marquee =  setInterval(() => {
+		buttons[i].classList.add("on");
+		console.log(i);
+		if(i < buttons.length - 1){
+			buttons[i + 1].classList.remove("on");
+		}else{
+			buttons[0].classList.remove("on");
+		}
+		i-- ;
+		if(i < 0){
+			i = buttons.length - 1;
+		}
+	}, 1000)
+
+}
+
+function pingPong(){
+	var buttons = document.querySelectorAll('[id=led]');
+	var i = 0;
+	var inc = 1;
+	marquee =  setInterval(() => {
+		
+		if(i >= buttons.length || i < 0){
+			i = i - inc
+			inc = inc * (-1)
+		}else{
+			if(i !== 0)
+				buttons[i-inc].classList.remove("on");
+			else
+				buttons[i].classList.remove("on");
+		}
+		buttons[i].classList.add("on");
+		console.log(i);
+		i = i + inc;
+
+	}, 1000)
+
+}
+
+//animateMarquee();
