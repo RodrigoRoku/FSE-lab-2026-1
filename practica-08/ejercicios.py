b1556fa45053fa1c516efa5e3ab3722fad7da090
RodrigoRoku <rodrigotana15@gmail.com>
2025-11-11 22:38:10 -0600

    Agregado el código para lel ejercicios 4 que realiza la lectura de temperatura y su posterior despligue en el display LCD

--- practica-08/ejercicios.py
+++ practica-08/ejercicios.py
@@ -1,7 +1,14 @@
 import lcdManager
 import utils
 from time import sleep
+import sensor
+
 lcd = lcdManager.lcdManager()
+
+try:
+  tempSensor = sensor.temperatureSensor()
+except:
+  print(e)
 
 def ejercicio1():
   lcd.setPosition(0,0)
@@ -14,8 +21,21 @@ def ejercicio2():
     lcd.sendCommand(0x01)
     lcd.printWord(palabra)
     sleep(1)
-  
 
-ejercicio1()
-sleep(2)
-ejercicio2()
+def ejercicio5():
+  nombre = "Gizell Alejo Rodrigo Tapia"
+  while True:
+    data = tempSensor.readTemp()
+    palabra = utils.formNextPrintableWord(nombre)
+    lcd.sendCommand(0x01)
+    lcd.setPosition(0,0)
+    lcd.printWord(palabra)
+    lcd.setPosition(1,4)
+    temp = f"Temp: {data.get('temp_c')} C"
+    lcd.printWord(temp)
+    sleep(1)
+
+# ejercicio1()
+# sleep(2)
+# ejercicio2()
+ejercicio5()
