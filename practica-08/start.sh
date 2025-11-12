c85e716ba744a0d95d751085d387d4fce4c9c399
RodrigoRoku <rodrigotana15@gmail.com>
2025-11-11 22:42:26 -0600

    Agregado script de arranque con las modificaciones necesarias para que ejecute el script de los ejercicios.

---
+++ practica-08/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+cd /home/pi || exit -1
+#if   [ -f test.pyc ]; then
+#	python3 test.pyc
+#elif [ -f test.py  ]; then
+#	python3 test.py
+#fi
+
+if   [ -f ejercicios.py ]; then
+	python3 ejercicios.py &
